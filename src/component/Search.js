0ca40c03b10d45e0d8abcccf134beb605b12308d
HAZberi <mhazuberi@gmail.com>
2020-12-30 00:07:14 -0700

    Search Component Part 1

--- src/component/Search.js
+++ src/component/Search.js
@@ -1,9 +1,52 @@
-import React , { useState } from 'react';
+import React, { useState, useEffect } from "react";
+import { AJAX } from "../helper.js";
 
 const Search = () => {
+  //State Definition/Initialization
+  const [term, setTerm] = useState("");
+  const [results, setResults] = useState([]);
+  //Updating State
+  useEffect(() => {
+    const search = async () => {
+      let url = "https://en.wikipedia.org/w/api.php?origin=*";
+      const params = {
+        action: "query",
+        list: "search",
+        format: "json",
+        srsearch: term,
+      };
+      Object.keys(params).forEach((key) => (url += `&${key}=${params[key]}`));
+      const data = await AJAX(url);
+      setResults(data.query.search);
+    };
+    //if there exists a search term otherwise set the results back to empty
+    term ? search() : setResults([]);
+  }, [term]);
+  //Render search list
+  const renderedListItems = results.map((result) => {
     return (
-        <h1>Welcome to Wikki Search</h1>
+      <div className="item" key={result.pageid}>
+        <div className="content">
+          <div className="header">{result.title}</div>
+          <span dangerouslySetInnerHTML={{ __html: result.snippet }}></span>
+        </div>
+      </div>
     );
-}
+  });
+  //Returns Search Component
+  return (
+    <div className="ui form">
+      <div className="field">
+        <label>Search</label>
+        <input
+          type="text"
+          value={term}
+          onChange={(e) => setTerm(e.target.value)}
+        />
+      </div>
+      <div className="ui celled list">{renderedListItems}</div>
+    </div>
+  );
+};
 
 export default Search;
