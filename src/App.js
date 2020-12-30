c549409dfa84d47b17e9dee0dd63eabadf9cf0e3
HAZberi <mhazuberi@gmail.com>
2020-12-29 21:10:50 -0700

    Accordian Component final

--- src/App.js
+++ src/App.js
@@ -1,9 +1,27 @@
 import React from "react";
+import "fomantic-ui-css/semantic.min.css";
 import Accordion from "./component/Accodion.js";
+
+const items = [
+  {
+    title: "What the hell is React?",
+    content: "React is a front-end javascript library used as a framework.",
+  },
+  {
+    title: "Why use React for front end applications?",
+    content: "React encapsulates are a lot of boring and repititive stuff.",
+  },
+  {
+    title: "Are there any frameworks compared to React?",
+    content:
+      "The choice is huge but you cant go wrong with React if you are a developer in 2021. Other honourable mentions for front-end frameworks are Angular and Vue.",
+  },
+];
+
 function App() {
   return (
     <div>
-      <Accordion />
+      <Accordion items={items}/>
     </div>
   );
 }
--- src/component/Accodion.js
+++ src/component/Accodion.js
@@ -1,7 +1,32 @@
-import React from "react";
+import React, { useState } from "react";
 
-const Accordion = () => {
-  return <h1>Accordion</h1>;
+const Accordion = ({ items }) => {
+  const [activeIndex, setActiveIndex] = useState(null);
+
+  const accordianTitleClick = (index) => {
+    setActiveIndex(index);
+  };
+  const renderedListItems = items.map((item, i) => {
+    const activeItem = i === activeIndex ? "active" : "";
+    return (
+      <React.Fragment key={item.title}>
+        <div
+          className={`${activeItem} title`}
+          onClick={() =>
+            activeItem ? accordianTitleClick(null) : accordianTitleClick(i)
+          }
+        >
+          <i className="dropdown icon"></i>
+          {item.title}
+        </div>
+        <div className={`${activeItem} content`}>
+          <p>{item.content}</p>
+        </div>
+      </React.Fragment>
+    );
+  });
+
+  return <div className="ui styled accordion">{renderedListItems}</div>;
 };
 
 export default Accordion;
